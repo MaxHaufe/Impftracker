9954a767d1c66ed563b9105a29d8862380edc080
max <max.haufe@htw-dresden.de>
2021-05-06 19:53:08 +0200

    corrected file path

--- main.py
+++ main.py
@@ -26,17 +26,16 @@ def setup_logger(name, log_file, level=logging.INFO):
 
 
 if __name__ == '__main__':
-    send_msg_flag = False
-    json_file = 'result.json'
+    # TODO hardcoded - issue
+    json_file = '/home/pi/impftracker/result.json'
 
     info_log = setup_logger("info_log", "info.log")
     result_log = setup_logger("result_log", "result.log")
 
-    info_log.info("Running...")
     response = requests.get("https://countee-impfee.b-cdn.net/api/1.1/de/counters/getAll/_iz_sachsen?cached=impfee")
 
     if response.status_code != 200:
-        print("Status Code: " + str(response.status_code))
+        info_log.info("FAIL")
         exit(-1)
     json_obj = json.loads(response.content)
 
@@ -51,10 +50,13 @@ if __name__ == '__main__':
 
     with open(json_file, 'w') as writer:
         json.dump(result, writer)
-        
+
     if data_from_file != result:
         message = ""
         for city, nr in result.items():
             message += "{:<12s}{:>10d}\n".format(city, nr)
 
+        result_log.info(result)
         telegram_send.send(messages=[message])
+
+    info_log.info("Running...SUCCESS")
